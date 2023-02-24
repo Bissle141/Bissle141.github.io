c3ba26e40e5a8e310a27ae5f03b8568512ff273d
Alyssa <alyssa.black141@gmail.com>
2023-02-24 01:25:20 -0700

    tb

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ window.addEventListener('load', () => {
             "desc": "Compendi is aimed at authors and writers looking for a place to organize and store their notes which is accessible from any device. With Compendi users can create project files with unlimited nested folders and files within them and they can find and store images in a multitude of ways.",
             "time_frame": "2 weeks",
             "tech_stack": ["Python", "Flask", "Flask-SQLAlchemy", "HTML", "Jinja", "CSS", "Cloudinary API", "Werkzeug"],
-            "demo_link": "https://www.youtube.com/embed/PdNTFznkjA0",
+            "demo_embed": `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/oBbiBo4XeeU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
             "repo_link": "https://github.com/Bissle141/Compendi"
         },
         
@@ -16,7 +16,7 @@ window.addEventListener('load', () => {
             "desc": "This project is a single-page full-stack application that I developed to showcase my learning during the first 8 weeks of my coding bootcamp at Devmountain. It encompasses a range of technical skills including front-end development using HTML, CSS, and JavaScript, as well as back-end development using Node.js and various databases. The application is designed to be dynamic and user-friendly, with a clean and modern interface. Overall, this project serves as a testament to my growing proficiency in full-stack development and my ability to apply newly acquired skills to real-world projects.",
             "time_frame": "1 weeks",
             "tech_stack": ["JavaScript", "HTML", "CSS", "NodeJS", "Express", "NPM", "JSON"],
-            "demo_link": "https://www.youtube.com/embed/oBbiBo4XeeU",
+            "demo_embed": `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/PdNTFznkjA0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
             "repo_link": "https://github.com/Bissle141/DragonsKeep"
         }
     
@@ -44,7 +44,7 @@ window.addEventListener('load', () => {
         techStackUl.innerHTML = techStackLi
 
         // Pop demo-link
-        demoVideo.setAttribute('src', projectList[projectIndex].demo_link)
+        demoVideo.innerHTML =projectList[projectIndex].demo_embed
 
         // Pop title
         projectTitleText.innerHTML = projectList[projectIndex].title
